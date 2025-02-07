599c9b658312ade5d1280f930aa44c510daac238
KennedyRyan-code <kenryan49@gmail.com>
2025-02-07 23:04:46 +0300

    Add publicId input and notFound exit to view-home controller

--- api/controllers/home/view-home.js
+++ api/controllers/home/view-home.js
@@ -3,15 +3,32 @@ module.exports = {
 
   description: 'Home index.',
 
-  inputs: {},
+  inputs: {
+    publicId: (
+      {
+        type: 'string',
+        description: 'Public ID',
+        required: false
+      }
+    )
+  },
 
   exits: {
     success: {
       responseType: 'inertia'
+    },
+    notFound: {
+      responseType: 'notFound'
     }
   },
 
-  fn: async function () {
+  fn: async function ({ publicId }) {
+    if (!publicId) {
+      return {
+        page: 'index',
+        props: { message: 'Open a Markdown file to get started.' }
+      }
+    }
     return { page: 'index' }
   }
 }
